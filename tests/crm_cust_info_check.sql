8b068d737709c801a6427b6f616c826fbc8ef817
Sushant Shirsekar <63793660+sushantshirsekar@users.noreply.github.com>
2025-11-09 13:36:53 +0530

    Create crm_cust_info_check.sql

---
+++ tests/crm_cust_info_check.sql
@@ -0,0 +1,43 @@
+/*==============================================================
+Script Name : Silver Layer Data Quality Checks - CRM Customer Info
+Purpose     : To validate data consistency and standardization in 
+              'silver.crm_cust_info'.
+Checks      :
+  1. Duplicates or NULLs in Primary Key (cst_id)
+  2. Unwanted spaces in text columns
+  3. Standard values for Gender & Marital Status
+Expected    : No invalid records found
+==============================================================*/
+
+-- Check for nulls or duplicates in Primary key
+-- Expectation : No Result
+SELECT 
+	cst_id, 
+	COUNT(*)
+FROM silver.crm_cust_info
+GROUP BY cst_id
+HAVING COUNT(*) > 1 OR cst_id IS NULL
+
+
+-- Check for unwanted spaces
+-- Expectation : No Result
+-- Use for different parameters -> cst_lastname,
+-- cst_marital_status, cst_gndr
+SELECT 
+	cst_id,
+	cst_firstname
+FROM silver.crm_cust_info
+WHERE cst_firstname != TRIM(cst_firstname)
+  
+
+-- Data Standardization and Consistency
+-- Expectation : 
+-- Gender -> Male, Female, Unknown
+-- Marital Status -> Single, Married, Unknown
+SELECT DISTINCT 
+	cst_gndr
+FROM silver.crm_cust_info
+
+SELECT DISTINCT 
+	cst_marital_status
+FROM silver.crm_cust_info
